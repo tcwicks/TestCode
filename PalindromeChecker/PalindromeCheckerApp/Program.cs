01b3cc1ac6632f8e44618dbca690ba12920e7e19
TCWicks <tcwicks@gmail.com>
2017-01-19 12:41:54 +1300

    Palindrome checker test app and unit tests

---
+++ PalindromeChecker/PalindromeCheckerApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCheckerApp
+{
+    public class Program
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (s == null) { return false; } // Otherwise we end up with an exception
+            string TrimmedText = s.Replace(@" ",string.Empty).ToLowerInvariant();
+            if (string.IsNullOrEmpty(TrimmedText)) { return false; } // Edge case - no idea if an empty string should be considered a Palindrome
+            char[] CharArray = TrimmedText.ToCharArray();
+            int MidPoint = CharArray.Length / 2; // Note that by default dot net truncates integer division results.
+            bool IsPalindromeMatch = true;
+            for (int I = 0; I < MidPoint; I++)
+            {
+                if (CharArray[I] != CharArray[CharArray.Length - (I+1)])
+                {
+                    IsPalindromeMatch = false;
+                    break; // Early exit
+                }
+            }
+            return IsPalindromeMatch;
+        }
+
+        static void Check(string s, bool shouldBePalindrome)
+        {
+            Console.WriteLine(IsPalindrome(s) == shouldBePalindrome ? "pass" : "FAIL");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Check("abcba", true);
+            Check("abcde", false);
+            Check("Mr owl ate my metal worm", true);
+            Check("Never Odd Or Even", true);
+            Check("Never Even Or Odd", false);
+
+            // Additional test cases
+            Check(null, false);
+            Check(string.Empty, false);
+            Check("Nev#er Odd Or E#ven", true);
+            Check("Nev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#ven", true);
+            Check("Nev#er O\r\ndd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd \n\rOr E#ven", true);
+
+            Console.ReadLine(); // Pause
+        }
+    }
+}
---
+++ PalindromeChecker/PalindromeCheckerUnitTests/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PalindromeCheckerUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCaseSimplePass()
+        {
+            Assert.AreEqual(true, PalindromeCheckerApp.Program.IsPalindrome(@"abcba"));
+        }
+        [TestMethod]
+        public void TestCaseSimpleFail()
+        {
+            Assert.AreEqual(false, PalindromeCheckerApp.Program.IsPalindrome(@"abcde"));
+        }
+        [TestMethod]
+        public void TestCaseComplexPass1()
+        {
+            Assert.AreEqual(true, PalindromeCheckerApp.Program.IsPalindrome(@"Mr owl ate my metal worm"));
+        }
+        [TestMethod]
+        public void TestCaseComplexPass2()
+        {
+            Assert.AreEqual(true, PalindromeCheckerApp.Program.IsPalindrome(@"Never Odd Or Even"));
+        }
+        [TestMethod]
+        public void TestCaseComplexFail1()
+        {
+            Assert.AreEqual(false, PalindromeCheckerApp.Program.IsPalindrome(@"Never Even Or Odd"));
+        }
+        [TestMethod]
+        public void TestEdgeCaseNull()
+        {
+            Assert.AreEqual(false, PalindromeCheckerApp.Program.IsPalindrome(null));
+        }
+        [TestMethod]
+        public void TestEdgeCaseEmptyString()
+        {
+            Assert.AreEqual(false, PalindromeCheckerApp.Program.IsPalindrome(string.Empty));
+        }
+        [TestMethod]
+        public void TestBlackBoxCasePunctuationPass()
+        {
+            Assert.AreEqual(true, PalindromeCheckerApp.Program.IsPalindrome(@"Nev#er Odd Or E#ven"));
+        }
+        [TestMethod]
+        public void TestBlackBoxCaseLongStringPass()
+        {
+            Assert.AreEqual(true, PalindromeCheckerApp.Program.IsPalindrome(@"Nev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd Or E#ven"));
+        }
+        [TestMethod]
+        public void TestBlackBoxCaseLongStringControlCharactersPass()
+        {
+            Assert.AreEqual(true, PalindromeCheckerApp.Program.IsPalindrome("Nev#er O\r\ndd Or E#venNev#er Odd Or E#venNev#er Odd Or E#venNev#er Odd \n\rOr E#ven"));
+        }
+    }
+}
